37689c55b56d60b94dc5a60eef44afe31bb3f7db
Lulus <snys98@outlook.com>
2021-07-30 10:44:03 +0800

    增加moduletestbase

---
+++ ModuleTestBase.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using AutoFixture;
+
+using Geex.Common.Abstraction;
+
+using ImpromptuInterface;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+using Mongo2Go;
+using MongoDB.Bson;
+using MongoDB.Entities;
+using Moq;
+
+using Volo.Abp;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Modularity;
+using Volo.Abp.Testing;
+
+namespace Geex.Common.Testing
+{
+    public abstract class ModuleTestBase<TStartupModule> : AbpIntegratedTest<TStartupModule>
+        where TStartupModule : IAbpModule
+    {
+        protected static Task<MongoDbRunner> db;
+        static ModuleTestBase()
+        {
+            db = Task.Run(() => MongoDbRunner.Start(singleNodeReplSet: true, singleNodeReplSetWaitTimeout: 5));
+        }
+        public string TestName = typeof(TStartupModule).Name + "Testing";
+        public static IFixture Fixture { get; protected set; } = new Fixture();
+
+        protected ModuleTestBase()
+        {
+        }
+
+        protected virtual async Task WithUow(Action action)
+        {
+            var uow = GetRequiredService<IUnitOfWork>();
+            action.Invoke();
+            await uow.CommitAsync();
+        }
+        protected virtual async Task WithUow(Func<Task> action)
+        {
+            var uow = GetRequiredService<IUnitOfWork>();
+            await action.Invoke();
+            await uow.CommitAsync();
+        }
+
+        protected override void AfterAddApplication(IServiceCollection services)
+        {
+            base.AfterAddApplication(services);
+            DB.MigrateAsync(this.GetType()).Wait();
+        }
+
+        protected override void SetAbpApplicationCreationOptions(AbpApplicationCreationOptions options)
+        {
+            Fixture.Register<string>(() => ObjectId.GenerateNewId().ToString());
+            options.UseAutofac();
+            Fixture.Register<IWebHostEnvironment>(() =>
+    new
+    {
+        ApplicationName = TestName,
+        EnvironmentName = "Testing"
+    }.ActLike<IWebHostEnvironment>());
+            options.Services.AddSingleton<IWebHostEnvironment>(Fixture.Create<IWebHostEnvironment>());
+            options.Services.Replace(ServiceDescriptor.Singleton(new GeexCoreModuleOptions()
+            {
+                AppName = TestName,
+                ConnectionString = db.Result.ConnectionString
+            }));
+        }
+
+    }
+}
